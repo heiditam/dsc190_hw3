87ecc9b5a50370ae14493c4fdcdb411fe7784079
ppagaduan <129784067+ppagaduan@users.noreply.github.com>
2024-11-10 10:57:27 -0800

    startng question 4

--- hw3.R
+++ hw3.R
@@ -1,10 +1,7 @@
 # read data
 data <- read.table("hcmv.txt", header = TRUE)
 head(data)
-
-N <- 200000
-gene <- seq(1, N)
-print(gene)
+length(data)
 
 # QUESTION 1
 num_sim = 100
@@ -55,4 +52,65 @@ triplet_diffs <- sapply(seq(1, ncol(distances) - 1, by = 3), function(i) rowSums
 for (i in 1:ncol(triplet_diffs)) {
   print(stripplot(triplet_diffs[,i], pch=16, cex=0.25)) # 1-D Scatter plot
 }
+
+# QUESTION 3
+
+DNA <- 1:N
+# INVESTIGATE REGIONS OF LENGTH 1000
+split_DNA <- split(DNA, ceiling(seq_along(DNA) / 1000))
+# function to count number of palindromes per region (observed counts)
+count_per_region <- function(region, palindrome_locations){
+  sum(region %in% data$location)
+}
+counts_1000 <- sapply(split_DNA, count_per_region, data$location)
+
+# number of palindromes we would expect for each region
+expected <- 296 / 230 # total palindromes / # chunks
+expected_vec <- rep(expected, 230)
+expected_prob <- expected_vec / sum(expected_vec)
+
+# perform a chi-squared test to compare observed counts with expected values
+chi_squared_test <- chisq.test(counts_1000, p = expected_prob)
+
+split_DNA_5k <- split(DNA, ceiling(seq_along(DNA) / 5000))
+counts_5000 <- sapply(split_DNA_5k, count_per_region, data$location)
+expected_5k <- 296 / 46 # total palindromes / # chunks
+
+expected_vec_5k <- rep(expected_5k, 46)
+expected_prob_5k <- expected_vec_5k / sum(expected_vec_5k)
+chi_squared_test_5k <- chisq.test(counts_5000, p = expected_prob_5k)
+
+split_DNA_10k <- split(DNA, ceiling(seq_along(DNA) / 10000))
+counts_10000 <- sapply(split_DNA_10k, count_per_region, data$location)
+expected_10k <- 296 / 23 # All conditions for the chi-squared test have been satisfied, described previously.
+expected_vec_10k <- rep(expected_10k, 23)
+expected_prob_10k <- expected_vec_10k / sum(expected_vec_10k)
+chi_squared_test_10k <- chisq.test(counts_10000, p = expected_prob_10k)
+
+library(ggplot2)
+library(forcats)
+# 
+normalized_1000 <- counts_1000 / 1000 * 1000
+normalized_5000 <- counts_5000 / 5000 * 1000
+normalized_10000 <- counts_10000 / 10000 * 1000
+
+palindrome_counts <- data.frame(
+  RegionLength = factor(rep(c('normalized_1000', 'normalized_5000', 'normalized_10000'),
+                            times = c(length(normalized_1000), length(normalized_5000), length(normalized_10000)))),
+  Count = c(normalized_1000, normalized_5000, normalized_10000)
+)
+# reorder DataFrame
+palindrome_counts$RegionLength <- factor(palindrome_counts$RegionLength, levels = c('normalized_1000', 'normalized_5000', 'normalized_10000'))
+
+ggplot(palindrome_counts, aes(x = RegionLength, y = Count, fill = RegionLength)) + 
+  geom_boxplot() + ylab('Density of Palindromes per 1,000 Base Pairs') + 
+  labs(title = 'Comparison of Palindrome Density for DNA Regions of Varying Lengths')
+
+# QUESTION 4
+library(ggplot2)
+
+
+ggplot(palindrome_counts, aes(x = RegionLength, y = Count, fill = RegionLength)) + 
+  geom_boxplot() + ylab('Density of Palindromes per 1,000 Base Pairs') + 
+  labs(title = 'Comparison of Palindrome Density for DNA Regions of Varying Lengths')
 
